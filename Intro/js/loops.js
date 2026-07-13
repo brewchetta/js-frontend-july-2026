e2a39bf2f0b1bef61270b069db92c22b117012ad
brewchetta <chett.tiller@gmail.com>
2026-07-13 15:29:17 -0400

    through loops

--- Intro/js/loops.js
+++ Intro/js/loops.js
@@ -2,7 +2,7 @@
 
 // a loop allows us to loop over a data structure (array) and do something with each item
 
-                        //    0    1      2      3     4
+                        //    0    1      2      3     4        5
 const shoppingCartPrices = [5.99, 7.99, 10.99, 30.99, 1.99]
 
 let sum = 0;
@@ -12,4 +12,61 @@ let sum = 0;
 // go through until we are at the last index
 let index = 0;
 
-while
+// while we have not gone past the length of the array...
+// add the cart price to the sum
+// and then move to the next cart price index
+while (index < shoppingCartPrices.length) {
+    // add price to the sum
+    // index is the current index for the item we're adding
+    const currentItem = shoppingCartPrices[index]
+    // add the current item to the sum and that's the new sum
+    sum = sum + currentItem
+
+    console.log("INDEX:", index)
+    console.log("CURRENTITEM:", currentItem)
+    console.log("NEW SUM:", sum)
+
+    // increment the index by one
+    index = index + 1
+}
+
+// GENERATE AN ARRAY OF RANDOM NUMBERS
+
+index = 0
+
+const randomNumbersArray = []
+
+while (index < 11) {
+    // generate a new random number
+    const randomNumber = Math.round( Math.random() * 10 )
+    // add that to the array
+    randomNumbersArray.push( randomNumber )
+
+    // increment the index
+    // index += 1
+    index++
+}
+
+console.log("randomNumbersArray:", randomNumbersArray)
+
+
+
+// FOR LOOPS //
+
+const randomNumbersArray2 = []
+
+// for (starting point; ending point; increment) {block}
+for (let index = 0; index < 11; index++) {
+    // generate a new random number
+    const randomNumber = Math.round( Math.random() * 10 )
+    // add that to the second random array
+    randomNumbersArray2.push( randomNumber )
+}
+
+console.log("randomNumbersArray2:", randomNumbersArray2)
+
+// counting down using a for loop
+for (let i = 10; i > 0; i--) {
+    console.log(i)
+}
+console.log("HAPPY NEW YEAR!!!")
